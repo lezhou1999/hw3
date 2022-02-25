23fd6687853411d98c4615cff4980747e86363f6
lezhou1999 <98565957+lezhou1999@users.noreply.github.com>
2022-02-25 08:57:46 -0500

    Add files via upload

--- PaintDryTimer.cpp
+++ PaintDryTimer.cpp
@@ -162,19 +162,12 @@ int main(){
 			TimeCode *timePointer = compute_time_code(sphereArea);
 			TimeCode tc = *timePointer;
 			TimeCode *tcPointer = &tc;
-
 			tcPointer = new TimeCode(*timePointer);
 			*tcPointer = tc;
-
-
 			newdss.name = "Batch- " + std::to_string(batchName);
 			cout << newdss.name;
 			newdss.timeToDry = tcPointer;
-			
-			
-			cout<<" will dry in:" <<newdss.timeToDry->ToString()<<endl;
-
-			
+			cout<<" will dry in:" <<tc.ToString()<<endl;			
 			toStore.push_back(newdss);
 			StoreTimeCode.push_back(tcPointer);
 			
